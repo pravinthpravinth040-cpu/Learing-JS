3325b421469776e346e6e6ac44ee128d10eb77b7
Pravinth N <pravinthpravinth040@gamil.com>
2026-08-04 17:28:27 +0530

    Modify

--- todo.js
+++ todo.js
@@ -1,10 +1,6 @@
 var fruit = ["Apple","Orange","Grape","Banana"]
 
-console.log(fruit[0]);
-console.log(fruit[1]);
-console.log(fruit[2]);
-console.log(fruit[3]);
-console.log(fruit[0]);
-console.log(fruit[1]);
-console.log(fruit[2]);
-console.log(fruit);
+for (count =0; count <= 3; count++){
+    console.log(fruit[count]);
+    
+}
