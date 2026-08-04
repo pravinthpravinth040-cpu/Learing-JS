6999d15d0607ebeee91978e8aaf6a71027a9b99c
Pravinth N <pravinthpravinth040@gamil.com>
2026-08-04 16:41:11 +0530

    Todoal

---
+++ todo.js
@@ -0,0 +1,10 @@
+var fruit = ["Apple","Orange","Grape","Banana"]
+
+console.log(fruit[0]);
+console.log(fruit[1]);
+console.log(fruit[2]);
+console.log(fruit[3]);
+console.log(fruit[0]);
+console.log(fruit[1]);
+console.log(fruit[2]);
+console.log(fruit);
